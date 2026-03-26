e15e397bc57bfe62de0cc1f32ad7147ea7c1b38f
wanqiupekdesign3 <wanqiupek3@gmail.com>
2026-03-26 18:12:32 +0800

    Add files via upload

---
+++ jscript.js
@@ -0,0 +1,6 @@
+ function changeText() {
+        const title = document.getElementById("title");
+        title.innerHTML = "you clicked the button!";
+        title.style.color = "blue";
+        alert("Text has been changed using inline javaScript!");
+    }
